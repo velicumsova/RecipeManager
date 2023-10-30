8ac75ea2efd56cf533c8fb6c2ea4b65c439bf0ff
smoothie-ws <kibot.email@gmail.com>
2023-10-30 13:49:29 +1000

    minor changes

--- src/main/java/com/example/recipemanager/controllers.java
+++ src/main/java/com/example/recipemanager/controllers.java
@@ -1,1 +1,31 @@
 package com.example.recipemanager;
+
+import javafx.fxml.FXML;
+import javafx.event.ActionEvent;
+import javafx.scene.control.Label;
+
+public class controllers {
+
+    @FXML
+    private Label myLabel;
+
+    @FXML
+    public void onRecipeListClick(ActionEvent event) {
+        myLabel.setText("Здесь будет список рецептов");
+    }
+
+    @FXML
+    public void onEditClick(ActionEvent event) {
+        myLabel.setText("Здесь будет меню для создания рецепта");
+    }
+
+    @FXML
+    public void onFavClick(ActionEvent event) {
+        myLabel.setText("Здесь будет список избранных рецептов");
+    }
+
+    @FXML
+    public void onCartClick(ActionEvent event) {
+        myLabel.setText("Здесь будет корзина");
+    }
+}
