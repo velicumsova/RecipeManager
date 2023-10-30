3ae859da9ab76235a1d325064dcb1f55bdeb7d0f
smoothie-ws <kibot.email@gmail.com>
2023-10-30 13:17:56 +1000

    third try

--- src/main/java/com/example/recipemanager/controllers.java
+++ src/main/java/com/example/recipemanager/controllers.java
@@ -1,12 +1,1 @@
 package com.example.recipemanager;
-import javafx.fxml.FXML;
-import javafx.scene.control.Label;
-
-public class controllers {
-    @FXML
-    private Label myLabel;
-    @FXML
-    protected void onRecipeListclick() {
-        myLabel.setText("Welcome to JavaFX Application!");
-    }
-}
