2ff6c212f26a5054d02bc77edad89ad5d87ccd03
smoothie-ws <kibot.email@gmail.com>
2023-11-05 17:04:48 +1000

    database update

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,6 +5,7 @@ module com.example.recipemanager {
     requires org.controlsfx.controls;
     requires org.kordamp.ikonli.javafx;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.recipemanager to javafx.fxml;
     exports com.example.recipemanager;
