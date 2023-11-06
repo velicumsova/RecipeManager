6cf2d16df862d73063e11d7222ea65f4a6b34b99
smoothie-ws <kibot.email@gmail.com>
2023-11-06 17:59:34 +1000

    minor changes

--- src/main/java/com/example/recipemanager/maincontrollers.java
+++ src/main/java/com/example/recipemanager/maincontrollers.java
@@ -4,30 +4,28 @@ import javafx.fxml.FXML;
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
 
-public class controllers {
-
+public class maincontrollers {
     @FXML
-    private Label myLabel;
+    private TabPane pagesList;
     @FXML
     private Button recipeListButton, makeRecipeButton, favButton, cartButton;
+
     @FXML
     public void onRecipeListClick(ActionEvent event) {
 
-            recipeListButton.getStyleClass().add("pressed");
-            recipeListButton.setStyle("-fx-opacity: 0.5");
-            myLabel.setText("Здесь будет список рецептов");
     }
     @FXML
     public void onMakeRecipeClick(ActionEvent event) {
-        myLabel.setText("Здесь будет меню для создания рецепта");
+
     }
     @FXML
     public void onFavClick(ActionEvent event) {
-        myLabel.setText("Здесь будет список избранных рецептов");
+
     }
     @FXML
     public void onCartClick(ActionEvent event) {
-        myLabel.setText("Здесь будет корзина");
+
     }
 }
