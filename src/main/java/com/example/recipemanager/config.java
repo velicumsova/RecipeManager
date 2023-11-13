910bd6c565338e14f121cc97076b8a8704746659
smoothie.ws <94678991+smoothie-ws@users.noreply.github.com>
2023-11-13 22:37:45 +1000

    Delete src/main directory

--- src/main/java/com/example/recipemanager/config.java
+++
@@ -1,9 +0,0 @@
-package com.example.recipemanager;
-
-public class config {
-    protected String dbHost = "localhost";
-    protected String dbPort = "3306";
-    protected String dbUser = "root";
-    protected String dbPass = "9090";
-    protected String dbName = "recipemanager";
-}
