245337f57fbad3573a2311df0be576ffa0b0c61b
smoothie-ws <kibot.email@gmail.com>
2023-11-13 23:01:18 +1000

    added mysql connection module

--- src/main/java/recipemanager/DataBaseHandler.java
+++ src/main/java/recipemanager/DataBaseHandler.java
@@ -4,7 +4,7 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 
-public class DataBaseHandler {
+public class DataBaseHandler extends Config{
     Connection dbConnection;
 
     public Connection getDbConnection() throws ClassNotFoundException, SQLException {
