1b54a1a84e8755241c16e0ced852e8be17b7873b
Eliza Velikasova <volkovaelizaveta347@gmail.com>
2023-12-08 15:06:51 +1000

    Reorganize project structure

--- src/main/java/recipemanager/MainApplication.java
+++ src/main/java/recipemanager/MainApplication.java
@@ -12,10 +12,10 @@ import java.io.IOException;
 public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("data/ui_form.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(MainApplication.class.getResource("/data/ui_form.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 1024, 768);
         stage.setTitle("Менеджер рецептов");
-        stage.getIcons().add(new Image(String.valueOf(MainApplication.class.getResource("data/icons/logo.png"))));
+        stage.getIcons().add(new Image(String.valueOf(MainApplication.class.getResource("/data/icons/logo.png"))));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
